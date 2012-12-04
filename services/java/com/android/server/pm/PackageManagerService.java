7f18578e200e51287941905e221301c560403a02
You Kim <you.kim72@gmail.com>
2012-12-04 23:57:46 +0900

    Remove unnecessary reference in PackageManagerService
    Change-Id: I55f40b8ded69e10bb940a7c84117c5ff1b988d8e
    Signed-off-by: You Kim <you.kim72@gmail.com>

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -553,7 +553,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         // Already bound to the service. Just make
                         // sure we trigger off processing the first request.
                         if (idx == 0) {
-                            mHandler.sendEmptyMessage(MCS_BOUND);
+                            sendEmptyMessage(MCS_BOUND);
                         }
                     }
                     break;
@@ -588,10 +588,9 @@ public class PackageManagerService extends IPackageManager.Stub {
                                         if (DEBUG_SD_INSTALL) Log.i(TAG,
                                                 "Posting delayed MCS_UNBIND");
                                         removeMessages(MCS_UNBIND);
-                                        Message ubmsg = obtainMessage(MCS_UNBIND);
                                         // Unbind after a little delay, to avoid
                                         // continual thrashing.
-                                        sendMessageDelayed(ubmsg, 10000);
+                                        sendEmptyMessageDelayed(MCS_UNBIND, 10000);
                                     }
                                 } else {
                                     // There are more pending requests in queue.
@@ -599,7 +598,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                                     // of next pending install.
                                     if (DEBUG_SD_INSTALL) Log.i(TAG,
                                             "Posting MCS_BOUND for next woek");
-                                    mHandler.sendEmptyMessage(MCS_BOUND);
+                                    sendEmptyMessage(MCS_BOUND);
                                 }
                             }
                         }
@@ -640,7 +639,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         // There are more pending requests in queue.
                         // Just post MCS_BOUND message to trigger processing
                         // of next pending install.
-                        mHandler.sendEmptyMessage(MCS_BOUND);
+                        sendEmptyMessage(MCS_BOUND);
                     }
 
                     break;
@@ -883,7 +882,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         }
 
                         processPendingInstall(args, ret);
-                        mHandler.sendEmptyMessage(MCS_UNBIND);
+                        sendEmptyMessage(MCS_UNBIND);
                     }
                     break;
                 }
@@ -921,7 +920,7 @@ public class PackageManagerService extends IPackageManager.Stub {
 
                         processPendingInstall(args, ret);
 
-                        mHandler.sendEmptyMessage(MCS_UNBIND);
+                        sendEmptyMessage(MCS_UNBIND);
                     }
 
                     break;
@@ -10286,8 +10285,8 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     /** Called by UserManagerService */
     void cleanUpUserLILPw(int userHandle) {
-        if (mDirtyUsers.remove(userHandle));
-        mSettings.removeUserLPr(userHandle);
+        if (mDirtyUsers.remove(userHandle))
+            mSettings.removeUserLPr(userHandle);
         if (mInstaller != null) {
             // Technically, we shouldn't be doing this with the package lock
             // held.  However, this is very rare, and there is already so much
