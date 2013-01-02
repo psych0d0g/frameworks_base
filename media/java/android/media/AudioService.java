b0c8c2235523561d5a804bba6536a0e855b0754e
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-01-02 21:20:16 +0100

    Feat: Allow users to disable safe headset volume warning (1/2)
    PS2: Fix Integer NPE
    
    Change-Id: I5da8e84215a7d6e4c2f9a1f21e7239826e6f9fda
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -482,14 +482,14 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                 null,
                 0);
 
+        mSafeMediaVolumeState = new Integer(SAFE_MEDIA_VOLUME_NOT_CONFIGURED);
+
         readPersistedSettings();
         mSettingsObserver = new SettingsObserver();
         updateStreamVolumeAlias(false /*updateVolumes*/);
         createStreamStates();
 
         mMediaServerOk = true;
-
-        mSafeMediaVolumeState = new Integer(SAFE_MEDIA_VOLUME_NOT_CONFIGURED);
 
         // Call setRingerModeInt() to apply correct mute
         // state on streams affected by ringer mode.
@@ -729,6 +729,7 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                     UserHandle.USER_CURRENT);
 
             readDockAudioSettings(cr);
+            updateManualSafeMediaVolume();
         }
         boolean linkNotificationWithVolume = Settings.System.getInt(mContentResolver,
                 Settings.System.VOLUME_LINK_NOTIFICATION, 1) == 1;
@@ -3461,6 +3462,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                 Settings.Global.DOCK_AUDIO_MEDIA_ENABLED), false, this);
             mContentResolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.VOLUME_LINK_NOTIFICATION), false, this);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.MANUAL_SAFE_MEDIA_VOLUME), false, this);
         }
 
         @Override
@@ -3504,6 +3507,7 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                     STREAM_VOLUME_ALIAS[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_NOTIFICATION;
                 }
                 readDockAudioSettings(mContentResolver);
+                updateManualSafeMediaVolume();
             }
         }
     }
@@ -5903,6 +5907,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
     // mSafeMediaVolumeDevices lists the devices for which safe media volume is enforced,
     private final int mSafeMediaVolumeDevices = AudioSystem.DEVICE_OUT_WIRED_HEADSET |
                                                 AudioSystem.DEVICE_OUT_WIRED_HEADPHONE;
+    // mManualSafeMediaVolume overrides the built-in safe media volume
+    boolean mManualSafeMediaVolume;
     // mMusicActiveMs is the cumulative time of music activity since safe volume was disabled.
     // When this time reaches UNSAFE_VOLUME_MUSIC_ACTIVE_MS_MAX, the safe media volume is re-enabled
     // automatically. mMusicActiveMs is rounded to a multiple of MUSIC_ACTIVE_POLL_PERIOD_MS.
@@ -5934,6 +5940,9 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
     }
 
     private void enforceSafeMediaVolume() {
+        // return if safe volume has been manually turned off
+        if (!mManualSafeMediaVolume) return;
+
         VolumeStreamState streamState = mStreamStates[AudioSystem.STREAM_MUSIC];
         boolean lastAudible = (streamState.muteCount() != 0);
         int devices = mSafeMediaVolumeDevices;
@@ -5975,7 +5984,7 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
             if ((mSafeMediaVolumeState == SAFE_MEDIA_VOLUME_ACTIVE) &&
                     (mStreamVolumeAlias[streamType] == AudioSystem.STREAM_MUSIC) &&
                     ((device & mSafeMediaVolumeDevices) != 0) &&
-                    (index > mSafeMediaVolumeIndex)) {
+                    (index > mSafeMediaVolumeIndex) && mManualSafeMediaVolume) {
                 mVolumePanel.postDisplaySafeVolumeWarning();
                 return false;
             }
@@ -6034,5 +6043,11 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
         pw.println("\nAudio routes:");
         pw.print("  mMainType=0x"); pw.println(Integer.toHexString(mCurAudioRoutes.mMainType));
         pw.print("  mBluetoothName="); pw.println(mCurAudioRoutes.mBluetoothName);
+    }
+
+    protected void updateManualSafeMediaVolume() {
+        mManualSafeMediaVolume = Settings.System.getBoolean(mContext.getContentResolver(),
+                Settings.System.MANUAL_SAFE_MEDIA_VOLUME, true);
+        setSafeMediaVolumeEnabled(mManualSafeMediaVolume);
     }
 }
