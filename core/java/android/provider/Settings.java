9d809c099eb3b62e0bd96db506b38c9b944a71d3
LorD ClockaN <davor@losinj.com>
2013-03-13 13:53:26 +0100

    Lockscreen see through (1/2) - CarbonRom
    -credit ParanoidAndroid
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3262,6 +3262,12 @@ public final class Settings {
         public static final String POWER_UI_LOW_BATTERY_WARNING_POLICY = "power_ui_low_battery_warning_policy";
 
         /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
