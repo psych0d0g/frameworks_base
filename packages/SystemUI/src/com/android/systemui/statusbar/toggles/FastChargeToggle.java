52ec0ccefd018666b45180ebdf8a51f1c5fa554c
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-04-05 19:09:49 +0200

    SystemUI: Add LongClick to FastCharge Toggle
    LongClick takes you to PerformanceControl
    
    Change-Id: I6f4de9bccb99a471ab8cf35144be8cc1235a8e70
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/toggles/FastChargeToggle.java

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/FastChargeToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/FastChargeToggle.java
@@ -1,12 +1,14 @@
 
 package com.android.systemui.statusbar.toggles;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.FileObserver;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
 
 import com.android.systemui.R;
 
@@ -131,6 +133,18 @@ public class FastChargeToggle extends StatefulToggle {
                 // ignore
             }
         }
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        Intent intent = new Intent("android.intent.action.MAIN");
+        intent.setComponent(ComponentName
+                .unflattenFromString("com.brewcrewfoo.performance/.activities.MainActivity"));
+        intent.addCategory("android.intent.category.LAUNCHER");
+
+        startActivity(intent);
+
+        return super.onLongClick(v);
     }
 
 }
