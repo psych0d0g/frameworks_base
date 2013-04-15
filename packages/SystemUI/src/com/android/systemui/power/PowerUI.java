ed87795554d954067082cc88f05eb4d222e26c37
LorD ClockaN <davor@losinj.com>
2013-04-15 22:19:00 +0200

    [1/2] Power connect/disconnect notification support - CM
    part 1/2: frameworks/base PowerUI and Settings
    http://review.cyanogenmod.org/#/c/35241/
    
    part 2/2: packages/apps/Settings Sound settings
    http://review.cyanogenmod.org/#/c/35242/
    
    patchset 3:
        shrink a couple of long lines
        remove power_notifications_use_default option (folded into ringtone option)
        load default settings in global instead of system
    
    patchset 5:
        support vibrate toggle
        ignore first power state change (on boot) if it's a DISCONNECT
    
    patchset 6:
        fix ignore first power state change
    
    Change-Id: I36a6b9f924d2cd52191a8e83a744745b37c5b068
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/power/PowerUI.java

--- packages/SystemUI/src/com/android/systemui/power/PowerUI.java
+++ packages/SystemUI/src/com/android/systemui/power/PowerUI.java
@@ -74,6 +74,9 @@ public class PowerUI extends SystemUI {
     AlertDialog mLowBatteryDialog;
     TextView mBatteryLevelTextView;
 
+    // For filtering ACTION_POWER_DISCONNECTED on boot
+    int mFirstPowerChangeEvent = 1;
+
     public void start() {
 
         mLowBatteryAlertCloseLevel = mContext.getResources().getInteger(
@@ -92,6 +95,7 @@ public class PowerUI extends SystemUI {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
         filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
         mContext.registerReceiver(mIntentReceiver, filter, null, mHandler);
     }
 
@@ -208,6 +212,10 @@ public class PowerUI extends SystemUI {
                 final boolean plugged = mPlugType != 0;
                 final boolean oldPlugged = oldPlugType != 0;
 
+                if (mFirstPowerChangeEvent > 0 && plugged) {
+                    mFirstPowerChangeEvent = 0;
+                }
+
                 int oldBucket = findBatteryLevelBucket(oldBatteryLevel);
                 int bucket = findBatteryLevelBucket(mBatteryLevel);
 
@@ -256,6 +264,25 @@ public class PowerUI extends SystemUI {
                     dismissLowBatteryNotificationWarning();
                 } else if (mShowLowBatteryDialogWarning && mBatteryLevelTextView != null) {
                     showLowBatteryWarning();
+                }
+            } else if (action.equals(Intent.ACTION_POWER_CONNECTED)
+                    || action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
+                final ContentResolver cr = mContext.getContentResolver();
+
+                switch (mFirstPowerChangeEvent) {
+                    case 1:
+                        mFirstPowerChangeEvent = 0;
+                        if (action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
+                            break;
+                        }
+                        // fallthrough
+                    case 0:
+                    default:
+                        if (Settings.Global.getInt(cr,
+                                Settings.Global.POWER_NOTIFICATIONS_ENABLED, 0)
+                                == 1) {
+                            playPowerNotificationSound();
+                        }
                 }
             } else {
                 Slog.w(TAG, "unknown intent: " + intent);
@@ -417,6 +444,34 @@ public class PowerUI extends SystemUI {
         d.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
         d.show();
         mInvalidChargerDialog = d;
+    }
+
+    void playPowerNotificationSound() {
+        final ContentResolver cr = mContext.getContentResolver();
+        final String soundPath =
+                Settings.Global.getString(cr, Settings.Global.POWER_NOTIFICATIONS_RINGTONE);
+
+        NotificationManager notificationManager =
+                (NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager == null) {
+            return;
+        }
+
+        Notification powerNotify=new Notification();
+        powerNotify.defaults = Notification.DEFAULT_ALL;
+        if (soundPath != null) {
+            powerNotify.sound = Uri.parse(soundPath);
+            if (powerNotify.sound != null) {
+                // DEFAULT_SOUND overrides so flip off
+                powerNotify.defaults &= ~Notification.DEFAULT_SOUND;
+            }
+        }
+        if (Settings.Global.getInt(cr,
+                Settings.Global.POWER_NOTIFICATIONS_VIBRATE, 1) == 0) {
+            powerNotify.defaults &= ~Notification.DEFAULT_VIBRATE;
+        }
+
+        notificationManager.notify(0, powerNotify);
     }
     
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
