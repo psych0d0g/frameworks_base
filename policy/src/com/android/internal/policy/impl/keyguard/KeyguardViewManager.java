0196e7b0898ea260be6a064012923e60434e8a6e
maxwen <max.weninger@gmail.com>
2013-09-03 01:45:27 +0200

    set correct keygard window flags also the first time
    Change-Id: I29d41a011d545fdfbeafb16eb31c26144f76fc7d

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
@@ -342,10 +342,16 @@ public class KeyguardViewManager {
 
             int flags = WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
                     | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
-                    | WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
-                    | WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER
                     | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
 
+            if (!mStatusbarHidden) {
+                flags |= WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN;
+            } else {
+                flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+            }
+            if (!mLoockThroughEnabled) {
+                flags |= WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
+            }
             if (!mNeedsInput) {
                 flags |= WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM;
             }
